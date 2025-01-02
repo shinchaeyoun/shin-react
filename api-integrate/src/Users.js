7f7a04a52fdbfd591bb998518346cceecd6e8853
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2025-01-02 16:24:32 +0900

    'push'

---
+++ api-integrate/src/Users.js
@@ -0,0 +1,152 @@
+/* eslint-disable */
+import React, { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+function Users1() {
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            // useEffect 에 첫번째 파라미터로 등록하는 함수에는 async 를 사용 할 수 없기 때문에 함수 내부에서 async 를 사용하는 새로운 함수를 선언해주어야 함.
+            try {
+                // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+                setError(null);
+                setUsers(null);
+                // loading 상태를 true 로 바꿉니다.
+                setLoading(true);
+                const response = await axios.get(
+                    'https://jsonplaceholder.typicode.com/users'
+                );
+                setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+            } catch (e) {
+                setError(e);
+            }
+            setLoading(false);
+        };
+
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!users) return null;
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>
+                    {user.username} ({user.name})
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+// 재로딩 버튼
+function Users2() {
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchUsers = async () => {
+        try {
+            // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+            setError(null);
+            setUsers(null);
+            // loading 상태를 true 로 바꿉니다.
+            setLoading(true);
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users'
+            );
+            setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!users) return null;
+    return (
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기</button>
+        </>
+    );
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "LOADING":
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case "SUCCESS":
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
+function Users() {
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null
+    });
+
+    const fetchUsers = async () => {
+        dispatch({ type: "LOADING" });
+
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            dispatch({ type: 'SUCCESS', data: response.data });
+        } catch (e) {
+            dispatch({ type: 'ERROR', error: e });
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const { loading, data: users, error } = state;
+
+    if (loading) return <div>Loading..</div>;
+    if (error) return <div>error!</div>;
+    if (!users) return null;
+
+    return (
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={fetchUsers}>retry</button>
+        </>
+    )
+}
+
+export default Users;
